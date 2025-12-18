af88b7f4dba731de8d6cd77368b495fbebf4b329
Szilard Nemeth <szilard.nemeth88@gmail.com>
2025-12-17 23:40:21 -0500

    Add spark-profiler to copy-source.sh

--- copy-source.sh
+++ copy-source.sh
@@ -1,2 +1,5 @@
 cp -R /Users/snemeth/development/cloudera/cde/spark-scale-and-perf/Spark_Inventory_API/*.py ./external_scripts/Spark_Inventory_API/
 ls -latr ./external_scripts/Spark_Inventory_API/
+
+cp -R /Users/snemeth/development/cloudera/cde/spark-scale-and-perf/spark-profiler/*.py ./external_scripts/spark-profiler/
+ls -latr ./external_scripts/spark-profiler/
